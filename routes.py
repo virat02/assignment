338075e77397cf380e416fc42064c77540f9a1e6
Virat Goradia <viratgoradia@Virat-Goradias-MacBook-Pro.local>
2021-11-21 20:49:56 -0500

    Basic setup done to test flask app routes to read from redis cache

--- routes.py
+++ routes.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from redisclient.redis_client import RedisClient
+
+# Initialize RedisClient
+redisClient = RedisClient()
+client = redisClient.getClient()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello'
+
+client.set('1', 'test-value-1')
+
+@app.route('/get/<key>')
+def get(key):
+    return client.get(key)
