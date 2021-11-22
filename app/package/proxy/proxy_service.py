0c63838e72b7f496ef63bc9176dac2f7d11e58bc
Virat Goradia <viratgoradia@Virat-Goradias-MacBook-Pro.local>
2021-11-22 16:59:34 -0500

    Inititalized LRUCache in proxy service

--- app/package/proxy/proxy_service.py
+++ app/package/proxy/proxy_service.py
@@ -5,23 +5,23 @@ class Proxy:
 
     def __init__(self):
         # Initialize RedisClient
-        self.redisClient = RedisClient()
+        self._redisClient = RedisClient()
 
         # Initialize LRU Cache
-        self.cache = LRUCache(4)
+        self._cache = LRUCache(4)
 
     def get(self, key):
-        val = self.cache.get(key)
+        val = self._cache.get(key)
 
         if val != -1:
             return val
         else:
-            val = self.redisClient.getVal(key)
+            val = self._redisClient.getVal(key)
             if val:
-                self.cache.put(key, val) 
+                self._cache.put(key, val) 
 
             return val
     
     def put(self, key, val):
-        return self.redisClient.put(key, val)
+        return self._redisClient.put(key, val)
 
